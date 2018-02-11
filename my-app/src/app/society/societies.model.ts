970fa9a78af7d0b1f51d38deb75c80d08a1d42c4
TANUJ DRALL <Tanuj9043>
2018-02-11 14:25:06 +0530

    Removed Extras

--- my-app/src/app/society/societies.model.ts
+++
@@ -1,9 +0,0 @@
-export class societies{
-  constructor(
-    public societyname:string,
-    public email:string,
-    public password:string,
-    public description:string,
-    public logo:string
-    ) { }
-}
--- my-app/src/app/user/users.model.ts
+++
@@ -1,7 +0,0 @@
-export class user{
-  constructor(
-    public username:string,
-    public email:string,
-    public password:string,
-    public imgsrc:string) { }
-}
